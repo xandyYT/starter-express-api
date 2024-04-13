1b545b29b0778ba7b328c97227050bd8e3cfb314
xandyYT <156695229+xandyYT@users.noreply.github.com>
2024-04-13 13:24:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+_path = process.cwd()
+
+var express = require("express")
+var cors = require("cors")
+var secure = require("ssl-express-www")
+
+var app = express()
+var mainrouter = require('./main.js')
+app.enable("trust proxy")
+app.set("json spaces", 2)
+app.use(cors())
+app.use(secure)
+app.use(express.static('public'))
+
+app.use("/", mainrouter)
+
+app.listen(process.env.PORT || 1406, () => {
+    console.log("ta on na porta 1406")
 })
-app.listen(process.env.PORT || 3000)
