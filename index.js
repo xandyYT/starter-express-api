e568757a8f131b8be1cd65f1bbd014bc48d2bd93
xandyYT <156695229+xandyYT@users.noreply.github.com>
2024-04-13 13:36:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set("json spaces", 2)
 app.use(cors())
 app.use(secure)
 app.use(express.static('public'))
-app.get('/eaer', async (req, res) => {
+app.all('/eaer', async (req, res) => {
     res.json({"oi": "oi"})
 })
 app.use("/", mainrouter)
